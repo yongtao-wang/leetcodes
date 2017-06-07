9c0d0b1b159ba4c0c2112a8e158816ce678387a1
パプリカ <steins@yw.T-mobile.com>
2017-06-06 21:07:58 -0700

    #3 done

--- algorithms/substring_and_palindrome.py
+++ algorithms/substring_and_palindrome.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class SubstringAndPalindrome(object):
     def lengthOfLongestSubstring(self, s):
         """
@@ -7,4 +8,14 @@ class SubstringAndPalindrome(object):
         :type s: str
         :rtype: int
         """
-        pass
+        '''NOTE: 1.使用max()函数代替if比较；2.若需要使用下标，就别用set'''
+        p = 0
+        max_length = 0
+        used = {}
+        for i in xrange(len(s)):
+            if s[i] in used and p <= used[s[i]]:
+                p = used[s[i]] + 1
+            else:
+                max_length = max(max_length, i - p + 1)
+            used[s[i]] = i
+        return max_length
--- try_out.py
+++ try_out.py
@@ -1,4 +1,5 @@
 from algorithms import list_processing as lp
+from algorithms import substring_and_palindrome as sp
 
 
 def try_1():
@@ -6,5 +7,10 @@ def try_1():
     print p.twoSum([3, 2, 4], 6)
 
 
+def try_3():
+    p = sp.SubstringAndPalindrome()
+    print p.lengthOfLongestSubstring('accaddddfghdkg')
+
 if __name__ == '__main__':
-    try_1()
+    # try_1()
+    try_3()
