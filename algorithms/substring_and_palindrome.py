9aa83d374911c1ff59df6e342921327e7b487d98
パプリカ <yongtao.wang.16@gmail.com>
2017-06-10 19:27:24 -0700

    #5 done

--- algorithms/substring_and_palindrome.py
+++ algorithms/substring_and_palindrome.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 class SubstringAndPalindrome(object):
+
     def lengthOfLongestSubstring(self, s):
         """
         # 3 Longest Substring Without Repeating Characters
@@ -19,3 +20,25 @@ class SubstringAndPalindrome(object):
                 max_length = max(max_length, i - p + 1)
             used[s[i]] = i
         return max_length
+
+    def longestPalindrome(self, s):
+        """
+        # 5 Given a string s, find the longest palindromic substring in s.
+        You may assume that the maximum length of s is 1000.
+        :type s: str
+        :rtype: str
+        """
+        if len(s) == 0:
+            return 0
+        max_length = 1
+        start = 0
+        for i in xrange(len(s)):
+            if i - max_length >= 1 and s[i - max_length - 1:i + 1] == s[i - max_length - 1:i + 1][::-1]:
+                start = i - max_length - 1
+                max_length += 2
+                continue
+
+            if i - max_length >= 0 and s[i - max_length:i + 1] == s[i - max_length:i + 1][::-1]:
+                start = i - max_length
+                max_length += 1
+        return s[start:start + max_length]
