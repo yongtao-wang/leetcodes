71789a7c62948398e80761fd9a6856ce8f1a63b8
パプリカ <yongtao.wang.16@gmail.com>
2017-06-11 18:18:13 -0700

    #8 regular expression matching

--- algorithms/strings.py
+++ algorithms/strings.py
@@ -41,3 +41,98 @@ class Strings(object):
                 return -0x80000000
 
         return base * sign
+
+    def isMatch(self, s, p):
+        """
+        # 10 regular expression matching
+
+        Implement regular expression matching with support for '.' and '*'.
+
+        '.' Matches any single character.
+        '*' Matches zero or more of the preceding element.
+
+        The matching should cover the entire input string (not partial).
+
+        The function prototype should be:
+        bool isMatch(const char *s, const char *p)
+
+        Some examples:
+        isMatch("aa","a") → false
+        isMatch("aa","aa") → true
+        isMatch("aaa","aa") → false
+        isMatch("aa", "a*") → true
+        isMatch("aa", ".*") → true
+        isMatch("ab", ".*") → true
+        isMatch("aab", "c*a*b") → true
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        '''
+        当j未溢出时，循环：
+            如果是最后一个字符
+                如果对应s[i]是最后一个字符：
+                    如果p[j]是'.'
+                        return True
+                    如果p[j]不是'.'
+                        return s[i] == p[j]
+                如果对应s[i]不是最后一个字符：
+                    return False
+            如果不是最后一个字符
+                如果p[j]是'.'
+                    如果紧跟着'*'
+                        如果这个'*'是最后一个字符
+                            True
+                        如果这个'*'不是最后一个字符
+                            # 这tm就麻烦了……得反向匹配了？
+                    如果没有跟着'*'
+                        匹配任意一个i（等于什么都不做，i += 1）
+                        j += 1
+                如果p[j]不是'.'
+                    如果紧跟着'*'
+                        循环直至不匹配i
+                        j += 2
+                    如果没有跟着'*'
+                        匹配一个i (if not s[i] == p[j] return False)
+                        j += 1
+                        
+        ----  leetcode讲解  ----
+        leet上大多为dp解法。此处暂时先写上。
+        
+        ----  此处为DFS解法 速度会明显慢一些  ----
+        
+        def isMatch(self, s, p):
+            cache = {}
+            if (s, p) in self.cache:
+                return self.cache[(s, p)]
+            if not p:
+                return not s
+            if p[-1] == '*':
+                if self.isMatch(s, p[:-2]):
+                    self.cache[(s, p)] = True
+                    return True
+                if s and (s[-1] == p[-2] or p[-2] == '.') and self.isMatch(s[:-1], p):
+                    self.cache[(s, p)] = True
+                    return True
+            if s and (p[-1] == s[-1] or p[-1] == '.') and self.isMatch(s[:-1], p[:-1]):
+                self.cache[(s, p)] = True
+                return True
+            self.cache[(s, p)] = False
+            return False
+            
+        -----------------------------------------
+        
+        '''
+        dp = [[False] * (len(s) + 1) for _ in range(len(p) + 1)]
+        dp[0][0] = True
+        for i in range(1, len(p)):
+            dp[i + 1][0] = dp[i - 1][0] and p[i] == '*'
+        for i in range(len(p)):
+            for j in range(len(s)):
+                if p[i] == '*':
+                    dp[i + 1][j + 1] = dp[i - 1][j + 1] or dp[i][j + 1]
+                    if p[i - 1] == s[j] or p[i - 1] == '.':
+                        dp[i + 1][j + 1] |= dp[i + 1][j]
+                else:
+                    dp[i + 1][j + 1] = dp[i][j] and (p[i] == s[j] or p[i] == '.')
+        return dp[-1][-1]
--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import unittest
 
 from algorithms import lists as lp
 from algorithms import numbers
+from algorithms import strings
 from algorithms import substring_and_palindrome as sp
 
 
@@ -10,6 +11,7 @@ class Run(unittest.TestCase):
         self.lp = lp.Lists()
         self.sp = sp.SubstringAndPalindrome()
         self.num = numbers.Numbers()
+        self.s_op = strings.Strings()
 
     def test_two_sum_leet1(self):
         self.assertEqual(self.lp.twoSum([3, 2, 4], 6), [1, 2])
@@ -19,12 +21,19 @@ class Run(unittest.TestCase):
 
     def test_longest_palindrome_leet5(self):
         self.assertEqual(self.sp.longestPalindrome('daabcbaddd'), 'abcba')
+        self.assertEqual(self.sp.longestPalindrome('aaaab'), 'aaaa')
 
     def test_reverse_integer_leet7(self):
         self.assertEqual(self.num.reverse(-3154), -4513)
-
-    def test_reverse_integer_overflow_leet7(self):
         self.assertEqual(self.num.reverse(1563847412), 0)
+
+    def test_regular_expression_leet8(self):
+        self.assertTrue(self.s_op.isMatch("aa", "aa"))
+        self.assertTrue(self.s_op.isMatch("aa", "a*"))
+        self.assertTrue(self.s_op.isMatch("ab", ".*"))
+        self.assertTrue(self.s_op.isMatch("aab", "c*a*b"))
+        self.assertFalse(self.s_op.isMatch("aaa","aa"))
+        self.assertFalse(self.s_op.isMatch("aa","a"))
 
 
 if __name__ == '__main__':
