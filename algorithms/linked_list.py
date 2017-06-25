4d3193e46efcbbdd0375cca8f7823ec1ce3cd06e
パプリカ <yongtao.wang.16@gmail.com>
2017-06-24 23:30:48 -0700

    leet21 merge two sorted linked list

--- algorithms/linked_list.py
+++ algorithms/linked_list.py
@@ -100,3 +100,32 @@ class LinkedList(object):
             p1 = p1.next
         p1.next = p1.next.next
         return head
+
+    def mergeTwoLists(self, l1, l2):
+        """
+        # 21. Merge Two Sorted Lists
+
+        Merge two sorted linked lists and return it as a new list.
+        The new list should be made by splicing together the nodes of the first two lists.
+
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        head = ListNode(0)
+        res = head
+        while l1 and l2:
+            if l1.val < l2.val:
+                head.next = l1
+                l1 = l1.next
+            else:
+                head.next = l2
+                l2 = l2.next
+            head = head.next
+
+        head.next = l1 or l2  # PYTHONIC!
+        return res.next
