b2d108267023dbb10ebc5432260989ef4780e299
パプリカ <yongtao.wang.16@gmail.com>
2017-07-04 23:49:07 -0700

    leet33 search in rotated sorted array

--- algorithms/lists.py
+++ algorithms/lists.py
@@ -323,6 +323,43 @@ class Lists(object):
                 nums[i:] = nums[i:][::-1]
                 return
 
+    def search(self, nums, target):
+        """
+        33. Search in Rotated Sorted Array
+        Suppose an array sorted in ascending order is rotated
+        at some pivot unknown to you beforehand.
+
+        (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+        You are given a target value to search.
+        If found in the array return its index, otherwise return -1.
+
+        You may assume no duplicate exists in the array.
+
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return -1
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) / 2
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid -1
+        return -1
+
 
 if __name__ == '__main__':
     # debug template
